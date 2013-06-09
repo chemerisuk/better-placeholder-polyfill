0ba1765cdd41cafc91cb70cb102cad3e66785221
chemerisuk <maksim.chemerisuk@strevus.com>
2013-06-09 09:32:31 +0300

    update better-dom version to ~1.0.0

--- src/better-placeholder-polyfill.js
+++ src/better-placeholder-polyfill.js
@@ -4,20 +4,20 @@
  *
  * Copyright (c) 2013 Maksim Chemerisuk
  */
-DOM.supports("placeholder", "input") || DOM.extend("[placeholder]", {
-    holder: "<input type='text' style='box-sizing: border-box; position: absolute; color: graytext; background: none no-repeat 0 0; border-color: transparent'/>"
-}, {
-    constructor: function(tpl) {
-        var offset = this.offset(),
-            holder = tpl.holder;
+DOM.supports("placeholder", "input") || DOM.extend("[placeholder]", [
+    "input[style='box-sizing: border-box; position: absolute; color: graytext; background: none no-repeat 0 0; border-color: transparent']"
+], {
+    constructor: function(holder) {
+        var offset = this.offset();
+
+        this
+            .on("focus", holder, "hide")
+            .on("blur", this._showPlaceholder, [holder]);
 
         holder
             .set(this.get("placeholder"))
             .setStyle("width", offset.right - offset.left)
-            .on("click", this.fire, ["focus"], this);
-
-        this.on("focus", holder.hide, [], holder);
-        this.on("blur", this._showPlaceholder, [holder]);
+            .on("click", this, "fire", ["focus"]);
 
         if (this.get() || this.isFocused()) holder.hide();
 
