74a7d330a5f574902cf7fb64aedae47e543fd339
chemerisuk <maksim.chemerisuk@strevus.com>
2013-07-04 23:21:19 +0300

    fix AMD support, add Gruntfile.js etc

--- src/better-placeholder-polyfill.js
+++ src/better-placeholder-polyfill.js
@@ -4,26 +4,38 @@
  *
  * Copyright (c) 2013 Maksim Chemerisuk
  */
-DOM.supports("placeholder", "input") || DOM.extend("[placeholder]", [
-    "input[style='box-sizing: border-box; position: absolute; color: graytext; background: none no-repeat 0 0; border-color: transparent']"
-], {
-    constructor: function(holder) {
-        var offset = this.offset();
-
-        this
-            .on("focus", holder, "hide")
-            .on("blur", this, "_showPlaceholder", [holder]);
-
-        holder
-            .set(this.get("placeholder"))
-            .setStyle("width", offset.right - offset.left)
-            .on("click", this, "fire", ["focus"]);
-
-        if (this.get() || this.isFocused()) holder.hide();
-
-        this.before(holder);
-    },
-    _showPlaceholder: function(holder) {
-        if (!this.get()) holder.show();
+(function(factory) {
+    if (typeof define === "function" && define.amd) {
+        define(["DOM"], factory);
+    } else {
+        factory(window.DOM);
     }
+})(function(DOM) {
+    "use strict";
+
+    if (DOM.supports("placeholder", "input")) return;
+
+    DOM.extend("[placeholder]", [
+        "input[style='box-sizing: border-box; position: absolute; color: graytext; background: none no-repeat 0 0; border-color: transparent']"
+    ], {
+        constructor: function(holder) {
+            var offset = this.offset();
+
+            this
+                .on("focus", holder, "hide")
+                .on("blur", this, "_showPlaceholder", [holder]);
+
+            holder
+                .set(this.get("placeholder"))
+                .setStyle("width", offset.right - offset.left)
+                .on("click", this, "fire", ["focus"]);
+
+            if (this.get() || this.isFocused()) holder.hide();
+
+            this.before(holder);
+        },
+        _showPlaceholder: function(holder) {
+            if (!this.get()) holder.show();
+        }
+    });
 });
