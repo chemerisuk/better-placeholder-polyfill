fc5ebf14c46e76140e9c3e732fff04a71d2b3b7f
Maksim Chemerisuk <chemerisuk@gmail.com>
2013-11-24 18:11:42 +0300

    version 1.1.0-rc.3

--- dist/better-placeholder-polyfill.js
+++ dist/better-placeholder-polyfill.js
@@ -1,6 +1,6 @@
 /**
  * @file src/better-placeholder-polyfill.js
- * @version 1.1.0-rc.2 2013-11-20T14:46:09
+ * @version 1.1.0-rc.3 2013-11-24T18:11:42
  * @overview [placeholder] polyfill for better-dom
  * @copyright Maksim Chemerisuk 2013
  * @license MIT
@@ -11,22 +11,31 @@
 
     if (typeof DOM.create("input").get("placeholder") === "string") return;
 
+    var PLACEHOLDER_KEY = "input-placeholder";
+
     DOM.extend("[placeholder]", {
         constructor: function() {
-            var holder = DOM.create("input[tabindex=-1 value='${v}' style='${css}']", {v: this.get("placeholder"), css: "box-sizing: border-box; position: absolute; color: graytext; background: none no-repeat 0 0; border-color: transparent"}),
-                offset = this.offset();
+            var placeholder = DOM.create("input[tabindex=-1 value='${v}' style='${css}']", {v: this.get("placeholder"), css: "box-sizing: border-box; position: absolute; color: graytext; background: none no-repeat 0 0; border-color: transparent"});
 
             this
-                .on("focus", function() { holder.hide() })
-                .on("blur", function() { if (!this.get()) holder.show() });
+                .on({focus: this.onFocus, blur: this.onBlur})
+                .data(PLACEHOLDER_KEY, placeholder)
+                .before(placeholder);
 
-            holder
-                .style("width", offset.right - offset.left)
-                .on("click", this, function() { this.fire("focus") });
+            placeholder
+                .style("width", this.width())
+                .on("click", this, this.onPlaceholderClick);
 
-            if (this.get() || this.matches(":focus")) holder.hide();
-
-            this.before(holder);
+            if (this.get() || this.matches(":focus")) placeholder.hide();
+        },
+        onFocus: function() {
+            this.data(PLACEHOLDER_KEY).hide();
+        },
+        onBlur: function() {
+            if (!this.get()) this.data(PLACEHOLDER_KEY).show();
+        },
+        onPlaceholderClick: function() {
+            this.fire("focus");
         }
     });
 }(window.DOM));
