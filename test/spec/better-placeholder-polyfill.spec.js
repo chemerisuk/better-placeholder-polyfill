6bf4e48a91b3d9690bbf38113edbe99c9d87b0c2
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-03-30 15:40:40 +0300

    update deps, add specs

--- test/spec/better-placeholder-polyfill.spec.js
+++ test/spec/better-placeholder-polyfill.spec.js
@@ -1,7 +1,40 @@
 describe("better-placeholder-polyfill", function() {
     "use strict";
 
-    it("should do something", function() {
-        // do testing here
+    var input, placeholder;
+
+    beforeEach(function() {
+        input = DOM.mock("input[placeholder='123']");
+        placeholder = DOM.mock();
+    });
+
+    it("should hide placeholder on focus", function() {
+        var spy = spyOn(placeholder, "hide");
+
+        input.onFocus(placeholder);
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it("should show placeholder on blur when value is empty", function() {
+        var spy = spyOn(placeholder, "show"),
+            getSpy = spyOn(input, "get").and.returnValue("123");
+
+        input.onBlur(placeholder);
+        expect(getSpy).toHaveBeenCalled();
+        expect(spy).not.toHaveBeenCalled();
+
+        getSpy.and.returnValue("");
+        getSpy.calls.reset();
+
+        input.onBlur(placeholder);
+        expect(getSpy).toHaveBeenCalled();
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it("should focus on input when placeholder was clicked", function() {
+        var spy = spyOn(input, "fire");
+
+        expect(input.onPlaceholderClick()).toBe(false);
+        expect(spy).toHaveBeenCalledWith("focus");
     });
 });
