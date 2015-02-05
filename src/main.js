c4e2ba08af9305d2227e20b38eb06c986c6eb422
Maksim Chemerisuk <chemerisuk@gmail.com>
2015-02-05 22:59:07 +0300

    various updates

---
+++ src/main.js
@@ -0,0 +1,50 @@
+(function(DOM) {
+    "use strict";
+
+    var test = DOM.create("input").get("placeholder");
+    var TEMPLATE = DOM.create("span.btr-placeholder[unselectable=on aria-hidden=true]");
+
+    DOM.extend("[placeholder]", typeof test !== "string", {
+        constructor() {
+            var placeholder = TEMPLATE.clone(true);
+
+            this.set("_placeholderElement", placeholder);
+
+            placeholder
+                .css(this.css(["width", "height", "font", "padding", "text-align", "border-width"]))
+                .on("mousedown", this._clickPlaceholder)
+                .set("textContent", this.get("placeholder"));
+
+            this
+                .on("focus", this._focusInput.bind(this, placeholder))
+                .on("blur", this._blurInput.bind(this, placeholder))
+                .before(placeholder);
+
+            if (this.get() || this.matches(":focus")) {
+                placeholder.css("display", "none");
+            }
+
+            this.define("placeholder",
+                () => placeholder.get("textContent"),
+                (value) => {
+                    placeholder.set("textContent", value);
+
+                    return value;
+                }
+            );
+        },
+        _focusInput(placeholder) {
+            placeholder.css("display", "none");
+        },
+        _blurInput(placeholder) {
+            if (!this.get()) {
+                placeholder.css("display", "");
+            }
+        },
+        _clickPlaceholder() {
+            this.fire("focus");
+
+            return false;
+        }
+    });
+}(window.DOM));
--- test/better-placeholder-polyfill.spec.js
+++ test/better-placeholder-polyfill.spec.js
@@ -6,45 +6,45 @@ describe("better-placeholder-polyfill", function() {
     beforeEach(function() {
         defineSpy = spyOn(Object, "defineProperty");
         input = DOM.mock("input[placeholder=`123`]");
-        placeholder = DOM.mock("span.placeholder");
+        placeholder = input.get("_placeholderElement");
     });
 
-    // it("should use placeholder value", function() {
-    //     expect(placeholder.get()).toBe("123");
+    it("should use placeholder value", function() {
+        expect(placeholder.get()).toBe("123");
 
-    //     input = DOM.mock("input[placeholder=`some text`]");
-    //     expect(input.get("placeholder")).toBe("some text");
-    // });
+        input = DOM.mock("input[placeholder=`some text`]");
+        expect(input.get("placeholder")).toBe("some text");
+    });
 
-    // it("displays placeholder if value exists", function() {
-    //     input = DOM.mock("input[placeholder=`123` value=`some value`]");
-    //     placeholder = input.get("_placeholder");
+    it("displays placeholder if value exists", function() {
+        input = DOM.mock("input[placeholder=`123` value=`some value`]");
+        placeholder = input.get("_placeholderElement");
 
-    //     expect(placeholder.get("aria-hidden")).toBe("true");
-    // });
+        expect(placeholder.get("aria-hidden")).toBe("true");
+    });
 
     it("shows placeholder on blur when value is empty", function() {
-        input.onFocus(placeholder);
+        input._focusInput(placeholder);
         expect(placeholder.css("display")).toBe("none");
 
-        input.onBlur(placeholder);
+        input._blurInput(placeholder);
         expect(placeholder.css("display")).not.toBe("none");
     });
 
     it("does not show placeholder if input has a value", function() {
-        input.onFocus(placeholder);
+        input._focusInput(placeholder);
         expect(placeholder.css("display")).toBe("none");
 
         input.set("123");
 
-        input.onBlur(placeholder);
+        input._blurInput(placeholder);
         expect(placeholder.css("display")).toBe("none");
     });
 
     it("should focus on input when placeholder was clicked", function() {
         var spy = spyOn(input, "fire");
 
-        expect(input.onPlaceholderClick()).toBe(false);
+        expect(input._clickPlaceholder()).toBe(false);
         expect(spy).toHaveBeenCalledWith("focus");
     });
 });
